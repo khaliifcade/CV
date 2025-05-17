e50a2b4b12d7835b575a65f7c01a3ed6fca593cf
Mohamed Khalif Ali <109949448+khaliifcade@users.noreply.github.com>
2025-05-17 20:41:22 +0300

    Add files via upload

---
+++ myfile.php
@@ -0,0 +1,9 @@
+<?php
+// Define string and number
+$my_string = "Hello, World!";
+$my_number = 42;
+
+// Print the string and the number
+echo "String: " . $my_string . "<br>";
+echo "Number: " . $my_number . "<br>";
+?>
